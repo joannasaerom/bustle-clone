69a1160c65b65602110bee6cd9c9bc3da7e175b8
Caleb Stevenson and Joanna Anderson <caleb@>
2016-10-25 11:21:39 -0700

    Adds initial files

---
+++ app/models/article.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  author: DS.attr(),
+  body: DS.attr(),
+  timestamp: DS.attr(),
+  isFeatured: DS.attr(),
+  image: DS.attr()
+});
